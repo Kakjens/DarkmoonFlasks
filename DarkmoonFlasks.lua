d41c4e783fe0cca15696c66cab2f442a623d6829
Kakjens <Kakjens@users.noreply.github.com>
2017-05-01 00:32:56 +0300

    Release candidate
    Breaks if hiding/displaying while in combat through Interface Options

--- DarkmoonFlasks.lua
+++ DarkmoonFlasks.lua
@@ -69,8 +69,160 @@ local function reset_position_reload()
 	ReloadUI();
 end
 
+
+			--local hide_if_none_at_bag = true
+			--local but_show_if_there_are_in_bank = true
+local private = {}
+private.defaults = {}
+private.defaults.DarkmoonFlasksCheckboxes = {
+	hide_if_none_at_bag = true,
+	but_show_if_there_are_in_bank = true,
+	frame_visible = true,
+}
+private.db = {}
+private.db.DarkmoonFlasksCheckboxes = {}
+
+
+local loader = CreateFrame("Frame")
+loader:RegisterEvent("ADDON_LOADED")
+	loader:SetScript("OnEvent", function(self, event, arg1)
+		if event == "ADDON_LOADED" and arg1 == "DejaCharacterStats" then
+			local function initDB(db, defaults)
+				if type(db) ~= "table" then db = {} end
+				if type(defaults) ~= "table" then return db end
+				for k, v in pairs(defaults) do
+					if type(v) == "table" then
+						db[k] = initDB(db[k], v)
+					elseif type(v) ~= type(db[k]) then
+						db[k] = v
+					end
+				end
+				return db
+			end
+			DarkmoonFlasksDB = initDB(DarkmoonFlasksDB, private.defaults)
+			private.db = DarkmoonFlasksDB
+			self:UnregisterEvent(event)
+		end
+	end)
+
+
+if false then
+--Open Categaories Fix
+do
+	local function get_panel_name(panel)
+		local tp = type(panel)
+		local cat = INTERFACEOPTIONS_ADDONCATEGORIES
+		if tp == "string" then
+			for i = 1, #cat do
+				local p = cat[i]
+				if p.name == panel then
+					if p.parent then
+						return get_panel_name(p.parent)
+					else
+						return panel
+					end
+				end
+			end
+		elseif tp == "table" then
+			for i = 1, #cat do
+				local p = cat[i]
+				if p == panel then
+					if p.parent then
+						return get_panel_name(p.parent)
+					else
+						return panel.name
+					end
+				end
+			end
+		end
+	end
+
+	local function InterfaceOptionsFrame_OpenToCategory_Fix(panel)
+		if doNotRun or InCombatLockdown() then return end
+		local panelName = get_panel_name(panel)
+		if not panelName then return end -- if its not part of our list return early
+		local noncollapsedHeaders = {}
+		local shownpanels = 0
+		local mypanel
+		local t = {}
+		local cat = INTERFACEOPTIONS_ADDONCATEGORIES
+		for i = 1, #cat do
+			local panel = cat[i]
+			if not panel.parent or noncollapsedHeaders[panel.parent] then
+				if panel.name == panelName then
+					panel.collapsed = true
+					t.element = panel
+					InterfaceOptionsListButton_ToggleSubCategories(t)
+					noncollapsedHeaders[panel.name] = true
+					mypanel = shownpanels + 1
+				end
+				if not panel.collapsed then
+					noncollapsedHeaders[panel.name] = true
+				end
+				shownpanels = shownpanels + 1
+			end
+		end
+		local Smin, Smax = InterfaceOptionsFrameAddOnsListScrollBar:GetMinMaxValues()
+		if shownpanels > 15 and Smin < Smax then 
+		  local val = (Smax/(shownpanels-15))*(mypanel-2)
+		  InterfaceOptionsFrameAddOnsListScrollBar:SetValue(val)
+		end
+		doNotRun = true
+		InterfaceOptionsFrame_OpenToCategory(panel)
+		doNotRun = false
+	end
+
+	hooksecurefunc("InterfaceOptionsFrame_OpenToCategory", InterfaceOptionsFrame_OpenToCategory_Fix)
+end
+	
+end
+	
+	
+	
 local DarkmoonFlasks
 DarkmoonFlasks = {};
+
+DarkmoonFlasks.panel = CreateFrame( "Frame", "DarkmoonFlasksPanel", UIParent );
+-- Register in the Interface Addon Options GUI
+-- Set the name for the Category for the Options Panel
+DarkmoonFlasks.panel.name = "DarkmoonFlasks";
+-- Add the panel to the Interface Options
+InterfaceOptions_AddCategory(DarkmoonFlasks.panel);
+
+-- Make a child panel
+-- DarkmoonFlasks.childpanel = CreateFrame( "Frame", "DarkmoonFlasksChild", DarkmoonFlasks.panel);
+-- DarkmoonFlasks.childpanel.name = "MyChild";
+-- Specify childness of this panel (this puts it under the little red [+], instead of giving it a normal AddOn category)
+-- DarkmoonFlasks.childpanel.parent = DarkmoonFlasks.panel.name;
+-- Add the child to the Interface Options
+-- InterfaceOptions_AddCategory(DarkmoonFlasks.childpanel);
+
+--Panel Title
+local DarkmoonFlasksDtitle=CreateFrame("Frame", "DarkmoonFlasksDtitle", DarkmoonFlasksPanel)
+	DarkmoonFlasksDtitle:SetPoint("TOPLEFT", 5, -5)
+	DarkmoonFlasksDtitle:SetScale(2.0)
+	DarkmoonFlasksDtitle:SetWidth(150)
+	DarkmoonFlasksDtitle:SetHeight(50)
+	DarkmoonFlasksDtitle:Show()
+
+local DarkmoonFlasksDtitleFS = DarkmoonFlasksDtitle:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+	DarkmoonFlasksDtitleFS:SetText('|cff00c0ffDarkmoonFlasks|r')
+	DarkmoonFlasksDtitleFS:SetPoint("TOPLEFT", 0, 0)
+	DarkmoonFlasksDtitleFS:SetFont("Fonts\\FRIZQT__.TTF", 10)
+	
+local DarkmoonFlasksDresetcheck = CreateFrame("Button", "DarkmoonFlasksDResetButton", DarkmoonFlasksPanel, "UIPanelButtonTemplate")
+	DarkmoonFlasksDresetcheck:ClearAllPoints()
+	DarkmoonFlasksDresetcheck:SetPoint("BOTTOMLEFT", 5, 5)
+	DarkmoonFlasksDresetcheck:SetScale(1.25)
+	DarkmoonFlasksDresetcheck:SetWidth(125)
+	DarkmoonFlasksDresetcheck:SetHeight(30)
+	_G[DarkmoonFlasksDresetcheck:GetName() .. "Text"]:SetText("Reset to Default")
+	DarkmoonFlasksDresetcheck:SetScript("OnClick", function (self, button, down)
+ 		--BeamMeUpDejaDBPC = private.defaults;
+		--ReloadUI();
+		DarkmoonFlasksDB = private.defaults
+		reset_position_reload()
+end)
 
 local DarkmoonFlasksDragFrame = CreateFrame("Frame", "DarkmoonFlasksDragFrame", UIParent)
 DarkmoonFlasksDragFrame:ClearAllPoints()
@@ -98,10 +250,13 @@ local h1 = h * 1.1
 local x
 local y
 
+local button_Count = 0
+
 local function createOneButton(itemID)
 	local _, itemLink, _,_,_,_,_,_,_,itemTexture,_ = GetItemInfo(itemID)
 	--print("create",itemLink)
 	if not _G["DarkmoonFlasksDButton"..itemID] then
+	button_Count = button_Count + 1
 	local DarkmoonFlasksDButton = CreateFrame("Button", "DarkmoonFlasksDButton"..itemID, DarkmoonFlasksDragFrame, "SecureActionButtonTemplate")
 	if stage[itemID] then y = h1 else y = 0 end
 	--x = (which[itemID] -1) * w1
@@ -136,13 +291,144 @@ local function createOneButton(itemID)
 	end)
 	DarkmoonFlasksDButton:HookScript("OnLeave", GameTooltip_Hide)
 	else
-		--print("trying to recreate",itemID)
+		print("trying to recreate",itemID)
 	end
 end
 
-local cache_writer = CreateFrame('Frame')
+local flask_State
+
+local function updateCount()
+	if button_Count == numIte then --is this check needed?
+		--print("flask state", flask_State)
+		if flask_State then
+			local hide_if_none_at_bag = private.db.DarkmoonFlasksCheckboxes.hide_if_none_at_bag
+			local but_show_if_there_are_in_bank = private.db.DarkmoonFlasksCheckboxes.but_show_if_there_are_in_bank
+			for index = 1, numIte, 1 do
+				local itemID = itemset[index]
+				local something = _G["DarkmoonFlasksDButton"..itemID]
+				local itemcount = GetItemCount(itemID, false, false)
+				--print(itemID, itemcount)
+				local itemcount_bank = GetItemCount(itemID, true, false)
+				something.DarkmoonFlasksDButtonFSD:SetFormattedText("%.0f", itemcount)
+				something.DarkmoonFlasksDButtonFSD:Show()
+				something.DarkmoonFlasksDButtonFSU:Hide()
+				if itemcount ~= itemcount_bank then
+					something.DarkmoonFlasksDButtonFSU:SetFormattedText("%.0f", itemcount_bank)
+					something.DarkmoonFlasksDButtonFSU:Show()
+				end
+				something:Show()
+				if itemcount_bank == 0 then
+					something:GetNormalTexture():SetDesaturated(true);
+				else
+					something:GetNormalTexture():SetDesaturated(false);
+				end
+				if hide_if_none_at_bag then
+					if itemcount == 0 then something:Hide() end
+				end
+				if but_show_if_there_are_in_bank then
+					if itemcount_bank > 0 then something:Show() end
+					if itemcount == 0 then
+						something:GetNormalTexture():SetDesaturated(true);
+					else
+						something:GetNormalTexture():SetDesaturated(false);
+					end
+				end
+			
+				--if hide_if_none_at_bag and but_show_if_there_are_in_bank then
+				--	if itemcount == 0 then
+				--		if itemcount_bank == 0 then
+				--			something:Hide()
+				--		else
+				--		end
+				--	end
+				--end
+				--end
+				_G["DarkmoonFlasksDButton"..itemID] = something
+			end
+		end
+	end
+end
+
+local DarkmoonFlasksHideCheck = CreateFrame("CheckButton", "DarkmoonFlasksHideCheck", DarkmoonFlasksPanel, "InterfaceOptionsCheckButtonTemplate")
+	DarkmoonFlasksHideCheck:RegisterEvent("ADDON_LOADED")
+	DarkmoonFlasksHideCheck:ClearAllPoints()
+	DarkmoonFlasksHideCheck:SetPoint("TOPLEFT", 25, -50)
+	DarkmoonFlasksHideCheck:SetScale(1.25)
+	_G[DarkmoonFlasksHideCheck:GetName() .. "Text"]:SetText("Hide icon for Darkmoon elixir if none in bag")
+	DarkmoonFlasksHideCheck.tooltipText = 'Checked hides icons for Darkmoon elixirs which are not in bag. Unchecked shows all possible Darkmoon elixirs.' --Creates a tooltip on mouseover.
+	
+	DarkmoonFlasksHideCheck:SetScript("OnEvent", function(self, button, up)
+		local checked = private.db.DarkmoonFlasksCheckboxes.hide_if_none_at_bag
+		self:SetChecked(checked)
+		private.db.DarkmoonFlasksCheckboxes.hide_if_none_at_bag = checked
+	end)
+
+	DarkmoonFlasksHideCheck:SetScript("OnClick", function(self, button, up)
+		private.db.DarkmoonFlasksCheckboxes.hide_if_none_at_bag = self:GetChecked()
+		updateCount()
+	end)
+	
+local DarkmoonFlasksShowCheck = CreateFrame("CheckButton", "DarkmoonFlasksShowCheck", DarkmoonFlasksPanel, "InterfaceOptionsCheckButtonTemplate")
+	DarkmoonFlasksShowCheck:RegisterEvent("ADDON_LOADED")
+	DarkmoonFlasksShowCheck:ClearAllPoints()
+	DarkmoonFlasksShowCheck:SetPoint("TOPLEFT", 40, -80)
+	DarkmoonFlasksShowCheck:SetScale(1.25)
+	_G[DarkmoonFlasksShowCheck:GetName() .. "Text"]:SetText("But display icon for Darkmoon elixir if it's in bank")
+	DarkmoonFlasksShowCheck.tooltipText = 'Checked displays icons for Darkmoon elixirs which are in bag or bank. Unchecked does nothing.' --Creates a tooltip on mouseover.
+	
+	DarkmoonFlasksShowCheck:SetScript("OnEvent", function(self, button, up)
+		local checked = private.db.DarkmoonFlasksCheckboxes.but_show_if_there_are_in_bank
+		self:SetChecked(checked)
+		private.db.DarkmoonFlasksCheckboxes.but_show_if_there_are_in_bank = checked
+	end)
+
+	DarkmoonFlasksShowCheck:SetScript("OnClick", function(self, button, up)
+		private.db.DarkmoonFlasksCheckboxes.but_show_if_there_are_in_bank = self:GetChecked()
+		updateCount()
+	end)	
+	
+
+local DarkmoonFlasksShowHide = CreateFrame("CheckButton", "DarkmoonFlasksShowHide", DarkmoonFlasksPanel, "InterfaceOptionsCheckButtonTemplate")
+	DarkmoonFlasksShowHide:RegisterEvent("ADDON_LOADED")
+	DarkmoonFlasksShowHide:ClearAllPoints()
+	DarkmoonFlasksShowHide:SetPoint("TOPLEFT", 25, -110)
+	DarkmoonFlasksShowHide:SetScale(1.25)
+	_G[DarkmoonFlasksShowHide:GetName() .. "Text"]:SetText("Toggle for displaying and hiding DarkmoonFlasks frame")
+	DarkmoonFlasksShowHide.tooltipText = 'Checked shows DarkmoonFlasks. Unchecked hides DarkmoonFlasks.' --Creates a tooltip on mouseover.
+	
+	DarkmoonFlasksShowHide:SetScript("OnEvent", function(self, button, up)
+		local checked = private.db.DarkmoonFlasksCheckboxes.frame_visible
+		self:SetChecked(checked)
+		private.db.DarkmoonFlasksCheckboxes.frame_visible = checked
+		if checked then
+			--DarkmoonFlasksDragFrame:Hide()
+			DarkmoonFlasksDragFrame:Show()
+			--DarkmoonFlasksShowHideButton:Show()
+		else
+			--DarkmoonFlasksDragFrame:Show()
+			DarkmoonFlasksDragFrame:Hide()
+			--DarkmoonFlasksShowHideButton:Hide()
+		end
+	end)
+	
+	DarkmoonFlasksShowHide:SetScript("OnClick", function(self, button, up)
+		local checked = self:GetChecked()
+		private.db.DarkmoonFlasksCheckboxes.frame_visible = checked
+		if checked then
+			--DarkmoonFlasksDragFrame:Hide()
+			DarkmoonFlasksDragFrame:Show()
+			--DarkmoonFlasksShowHideButton:Show()
+		else
+			--DarkmoonFlasksDragFrame:Show()
+			DarkmoonFlasksDragFrame:Hide()
+			--DarkmoonFlasksShowHideButton:Hide()
+		end
+	end)	
+
+
 local wait = {}
 
+local cache_writer = CreateFrame('Frame')
 cache_writer:SetScript('OnEvent', function(self, event, ...)
 	if event == 'GET_ITEM_INFO_RECIEVED' then
 		-- the info is now downloaded and cached
@@ -154,37 +440,7 @@ cache_writer:SetScript('OnEvent', function(self, event, ...)
 		end
 	end
 end)
-
 cache_writer:RegisterEvent('GET_ITEM_INFO_RECIEVED')
-
-local function updateCount()
-	for index = 1, numIte, 1 do
-		local itemID = itemset[index]
-		--local _, itemLink = GetItemInfo(itemID)
-		--print(itemLink)
-		--if itemLink then -- on unseen items error
-		if _G["DarkmoonFlasksDButton"..itemID] then
-			local something = _G["DarkmoonFlasksDButton"..itemID]
-			local itemcount = GetItemCount(itemID, false, false)
-			--print(itemID, itemcount)
-			local itemcount_bank = GetItemCount(itemID, true, false)
-			something.DarkmoonFlasksDButtonFSD:SetFormattedText("%.0f", itemcount)
-			something.DarkmoonFlasksDButtonFSD:Show()
-			something.DarkmoonFlasksDButtonFSU:Hide()
-			if itemcount ~= itemcount_bank then
-				something.DarkmoonFlasksDButtonFSU:SetFormattedText("%.0f", itemcount_bank)
-				something.DarkmoonFlasksDButtonFSU:Show()
-			end
-			--something:Show()
-			if itemcount_bank == 0 then
-				something:GetNormalTexture():SetDesaturated(true);
-			else
-				something:GetNormalTexture():SetDesaturated(false);
-			end
-			_G["DarkmoonFlasksDButton"..itemID] = something
-		end
-	end
-end
 
 local function createButtons()
 	for index = 1, numIte, 1 do
@@ -197,7 +453,6 @@ local function createButtons()
 			wait[itemID] = {}
 		end
 	end
-	updateCount()
 end
 
 local DarkmoonFlasksInitFrame = CreateFrame("Frame", "DarkmoonFlasksInitFrame", DarkmoonFlasksDragFrame)
@@ -210,15 +465,9 @@ DarkmoonFlasksInitFrame:SetScript("OnEvent", function(self, event, ...)
 			local name = GetItemInfo(itemID)
 			stage[itemID] = where_stage
 			where_stage = not where_stage
-			--which[itemID] = floor(index+temp)/2
 			which[itemID] = ceil(index/2)
 		end
-		C_Timer.After(5, createButtons)
-		--for some reason looking up the items during login fixes non-creation of buttons
-		--updateCount()
-		--print("Darkmoon flasks init login")
-		--self:RegisterEvent("BAG_UPDATE")
-		self:RegisterEvent("BAG_UPDATE_DELAYED")
+		--self:UnregisterUvent(event)
 	end
 	--if event == "BAG_UPDATE" then
 	if event == "BAG_UPDATE_DELAYED" then
@@ -299,7 +548,7 @@ SlashCmdList["DarkmoonFlasks"] = DarkmoonFlasks.SlashCmdHandler;
 -- GetItemInfo(itemID or "itemString" or "itemName" or "itemLink")
 
 local DarkmoonFlasksShowHideButtontooltipText
-local flask_State
+
 local function set_showhide_state(state)
 	if state then
 		DarkmoonFlasksShowHideButtontooltipText = "Shows buttons for Darkmoon Draughts and Tinctures in your bags." --Creates a tooltip on mouseover.
@@ -311,41 +560,40 @@ local function set_showhide_state(state)
 	flask_State = not state
 end
 local function toggle_showhide_state()
-	--print(flask_State,"flask state")
-	set_showhide_state(flask_State)
-	--flask_State = not flask_State
+	set_showhide_state(flask_State) --seems like it can be used for toggling
 end
 
 local function darkmoon_hide()
 	--print("reseting frames")
 	--pairs don't preserve order
-	for index =1, numIte, 1 do
+	for index = 1, numIte, 1 do
 		local itemID = itemset[index]
 		--print(index,itemname,"loop")
 		local something = _G["DarkmoonFlasksDButton"..itemID]
-		--print(something)
 		if something:IsShown() then something:Hide() end
 		_G["DarkmoonFlasksDButton"..itemID]=something
 	end
 end
 
+local need_Buttons = true
 local function darkmoon_show()
-	--print("showing frames")
 	--pairs don't preserve order
-	for index =1, numIte, 1 do
+	if need_Buttons then
+		createButtons()
+		need_Buttons = false
+		DarkmoonFlasksInitFrame:RegisterEvent("BAG_UPDATE_DELAYED")
+	end
+	for index = 1, numIte, 1 do
 		local itemID = itemset[index]
-		--print(index,itemname,"loop")
 		if not _G["DarkmoonFlasksDButton"..itemID] then
 			--print("needed",itemID)
 			createOneButton(itemID)
-			--createButtons()
-			--if not _G["DarkmoonFlasksDButton"..itemID] then print("yay") end
 		end
 		local something = _G["DarkmoonFlasksDButton"..itemID]
-		--print(something)
 		something:Show()
 		_G["DarkmoonFlasksDButton"..itemID]=something
 	end
+	--print("Darkmoon show")
 	updateCount()
 end
 
@@ -371,12 +619,11 @@ local function darkmoon_visibility_change()
 			print("buttons will be displayed after exiting the combat")
 		end
 	else
-		if flask_State then darkmoon_hide() else darkmoon_show() end
 		toggle_showhide_state()
+		if flask_State then darkmoon_show() else darkmoon_hide() end
 		DarkmoonFlasksShowHideButton_OnEnter()
 	end
 end
-
 
 updater:SetScript("OnEvent", function(self, event,arg1)
 	if event == "PLAYER_REGEN_ENABLED" then
@@ -384,9 +631,6 @@ updater:SetScript("OnEvent", function(self, event,arg1)
 		self:UnregisterEvent(event)
 	end
 end)				
-
-
-
 
 local DarkmoonFlasksShowHideButton = CreateFrame("Button", "DarkmoonFlasksShowHideButton", DarkmoonFlasksDragFrame, "UIPanelButtonTemplate")
 DarkmoonFlasksShowHideButton:RegisterEvent("ADDON_LOADED")
@@ -401,16 +645,11 @@ DarkmoonFlasksShowHideButton:SetScript("OnLeave", DarkmoonFlasksShowHideButton_O
 DarkmoonFlasksShowHideButton:SetScript("OnEvent", function(self, event, arg1)
 	if arg1 == addonname then
 		set_showhide_state(true)
-		--DarkmoonFlasksShowHideButtontooltipText = "Show buttons for Darkmoon Draughts and Tinctures in your bags." --Creates a tooltip on mouseover.
-		--_G[DarkmoonFlasksShowHideButton:GetName() .. "Text"]:SetText("Show")
 	end
 end)
 
 DarkmoonFlasksShowHideButton:SetScript("OnClick", function(self, button, up)
-	--print(flask_State,"flask_State on click")
 	darkmoon_visibility_change()
-	--DarkmoonFlasksShowHideButton_OnEnter() --might change tooltips on combat despite not doing anything
-	--print("showhideon enter_click")
 end)
 
 --local breaktext = "Creates error message"--Creates a tooltip on mouseover.	
